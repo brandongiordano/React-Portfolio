efd773a8d974a38a0566cbf347de4370a5eff64b
brandongiordano <bgiordano9917@aol.com>
2023-04-10 13:07:01 -0400

    added mobile drop down nav

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BG from '../assets/B.png'
+import { FiMenu } from "react-icons/fi"
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#003049] text-white">
             <div>
                 <img src={BG} alt="BG Logo" style={{width: '80px'}} />
             </div>
             <div>
-                <ul className="flex">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
+                <ul className="hidden md:flex">
+                    <li className='hover:text-[#d62828] duration-100'>Home</li>
+                    <li className='hover:text-[#d62828] duration-100'>About</li>
+                    <li className='hover:text-[#d62828] duration-100'>Skills</li>
+                    <li className='hover:text-[#d62828] duration-100'>Projects</li>
+                    <li className='hover:text-[#d62828] duration-100'>Contact</li>
+                </ul>
+
+                <div onClick={handleClick} className='md:hidden z-40'>
+                    <FiMenu />
+                </div>
+
+                {/* Mobile Only Nav */}
+                <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#003049] flex flex-col justify-center items-center"}>
+                    <li className='py-5 text-4xl'>Home</li>
+                    <li className='py-5 text-4xl'>About</li>
+                    <li className='py-5 text-4xl'>Skills</li>
+                    <li className='py-5 text-4xl'>Projects</li>
+                    <li className='py-5 text-4xl'>Contact</li>
                 </ul>
             </div>
         </div>
