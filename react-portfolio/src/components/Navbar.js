29df20110f97ed476a951d3af50f39180c23bbd5
brandongiordano <bgiordano9917@aol.com>
2023-04-10 10:38:35 -0400

    playing with styling

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import BG from '../assets/B.png'
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#003049] text-[#d62828]">
-          Navbar
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#003049] text-white">
+            <div>
+                <img src={BG} alt="BG Logo" style={{width: '80px'}} />
+            </div>
+            <div>
+                <ul className="flex">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
       );
 }
