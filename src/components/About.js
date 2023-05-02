edae05b95999cdba514a3ac39cf19dc6dc93de9c
brandongiordano <bgiordano9917@aol.com>
2023-05-01 21:28:31 -0400

    added new project

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ const About = () => {
     return (
         <div name="about" className="bg-[#003049] mx-auto p-4 flex px-10 flex-col justify-center w-full h-full md:h-screen">
             <div className="w-full md:w-1/2">
-            <h1 className="text-5xl py-1 text-white font-bold inline border-b-4 border-[#d62828]" id="about-title">
+              <h1 className="text-5xl py-1 text-white font-bold inline border-b-4 border-[#d62828]" id="about-title">
               About Me
-            </h1>
-            <div className='w-full pt-10 grid grid-cols-2 gap-3 justify-around'>
+              </h1>
+              <div className='w-full pt-10'>
                 <img src={ME} className='rounded-lg' alt="My Jiu Jitsu class" />
-                <p className="mb-8 py-8 text-[white]" id="about-content">
+                  <p className="mb-8 py-8 text-[white]" id="about-content">
               I'm a Full Stack Web Developer based in Rockledge, Florida. I was originally born in Queens, NY but moved down to the sunshine state with my family in 2009. I have completed
               two college degrees such as an Associates in Arts as well as an
               Associates in Science from Eastern Florida State College. I have a
@@ -20,8 +20,8 @@ const About = () => {
               but I'm eager to learn and grow in tech. My favourite hobby is
               teaching and training Jiu Jitsu and MMA but I'm also a
               huge gamer and love movies.
-                </p>
-            </div>
+                  </p>
+              </div>
             </div>
         </div>
     )
