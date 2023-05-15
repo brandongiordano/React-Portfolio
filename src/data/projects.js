18265e8e339767f25a76d3e67c79698b358cc662
brandongiordano <bgiordano9917@aol.com>
2023-05-15 10:43:48 -0400

    new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 import WMCpic from '../assets/WMCpic.png';
 import FlickPic from '../assets/FlickPic.png';
+import StorePic from '../assets/store.png';
 
 export const data=[
     {
@@ -16,15 +17,15 @@ export const data=[
         img:FlickPic,
         github:"https://github.com/AveryNewhart/The-Flick-Tracker",
         live:"https://the-flick-tracker.herokuapp.com/",
-        description:"A MERN stack social media platform that allows user to create watched and watchlists and follow other users to view theirs."
+        description:"A MERN stack movie-based social media platform that allows user to create watched and watchlists and follow other users to view theirs. Still a work in progress."
     },
     {
         id:3,
-        name:"Who's My Congressman?",
-        img:WMCpic,
-        github:"https://github.com/brandongiordano/whos-my-congressman",
-        live:"https://brandongiordano.github.io/whos-my-congressman/",
-        description:"A simple App utilizing the Google Civic API. Allows a user to type in their address and recieve their local Representative and Senators."
+        name:"Your Store Here",
+        img:StorePic,
+        github:"https://github.com/brandongiordano/your-store-here",
+        live:"",
+        description:"A MERN eccomerce site using Stripe as a payment platform. Features user authentication and a responsive single page design. Not deployed at the moment."
     },
     {
         id:4,
